666a138e1708fbffcad51a40105a84710d1e9ff8
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-10-05 15:55:58 +0200

    Created IDatabaseService interface and service as well.

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using ServiceContracts;
+using Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Services
+{
+    public class DatabaseService : IDatabaseService
+    {
+        private readonly AppDbContext _appDbContext;
+        public DatabaseService(AppDbContext appDbContext) 
+        {
+            _appDbContext = appDbContext;
+        }
+        public (bool IsConnected, string? ErrorMessage) CanConnect()
+        {
+            try
+            {
+                using (_appDbContext.Database.GetDbConnection())
+                {
+                    _appDbContext.Database.OpenConnection();
+                    bool IsConnected = _appDbContext.Database.CanConnect();
+                    _appDbContext.Database.CloseConnection();
+
+                    return (true, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+    }
+}
