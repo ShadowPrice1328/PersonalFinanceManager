eb4ce0c79e71c599b5c8175d851f226c656bb6b5
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-10-10 20:48:57 +0200

    Merget Auth and Main layout

---
+++ Personal_Finance_Manager/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Personal_Finance_Manager.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Personal_Finance_Manager/wwwroot/js/setactive.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("ul li a");
+    const currentPath = window.location.pathname;
+
+    navLinks.forEach(function (link) {
+        if (link.getAttribute("href") === currentPath) {
+            link.classList.add("active");
+        }
+
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            
+            navLinks.forEach(function (link) {
+                link.classList.remove("active");
+            });
+
+            this.classList.add("active");
+
+            window.location.href = this.getAttribute("href");
+        });
+    });
+});
