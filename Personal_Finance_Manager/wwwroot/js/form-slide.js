d4ac098993bfb8e6b4e149941061b3bfa01ba9a5
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-10-15 21:34:13 +0200

    Created a separate js file for Login form sliding.

---
+++ Personal_Finance_Manager/wwwroot/js/form-slide.js
@@ -0,0 +1,14 @@
+﻿let authenticated = document.getElementById('authenticated').value;
+console.log(authenticated);
+
+function toggleLoginForm(authenticated) {
+    const loginForm = document.getElementById('loginForm');
+    loginForm.classList.toggle('slide');
+}
+
+window.onload = function () {
+    if (authenticated) {
+        console.log(authenticated);
+        document.getElementById('loginForm').classList.add('slide');
+    }
+};
