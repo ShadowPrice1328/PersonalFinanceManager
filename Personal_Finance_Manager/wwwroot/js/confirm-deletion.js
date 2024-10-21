e332c785da9ea8d4b770a6a534da34233a791f01
Anna Sonchak <kisukeurahara1328@gmail.com>
2024-10-21 18:59:29 +0200

    Avoided offline malfunctionality of webpages. Modified Delete Category page.

---
+++ Personal_Finance_Manager/wwwroot/js/confirm-deletion.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    const initialDetailsContent = $(".details-section dl").html();
+
+    $("#delete-btn").on("click", function (e) {
+        if ($("#delete-btn").val() !== "Yes") {
+            console.log("Not yes, default...");
+
+            e.preventDefault();
+
+            $(".details-section dl").css("margin-left", "3rem");
+            $(".details-section dl").css("grid-template-columns", "1fr 1fr");
+            $(".details-section dl").html("<h2>Are you sure?</h2>");
+
+            $("#delete-btn").val("Yes");
+            $("#back-to-list-btn").text("No");
+        } else {
+            console.log("yes!");
+        }
+    });
+
+    $("#back-to-list-btn").on("click", function (e) {
+        if ($("#delete-btn").val() === "Yes") {
+            e.preventDefault();
+
+            $(".details-section dl").css("margin-left", "1.5rem");
+            $(".details-section dl").css("grid-template-columns", "1fr 2fr");
+
+            $(".details-section dl").html(initialDetailsContent);
+
+            $("#delete-btn").val("Delete");
+            $("#back-to-list-btn").text("Back");
+        }
+    });
+});
